03d811971df1d780fb54ff97a5323f92554d7615
giaodaica <trunghieu042000@gmail.com>
2025-04-27 15:39:37 +0700

    first commit

---
+++ resources/views/card/head.blade.php
@@ -0,0 +1,24 @@
+<head>
+    <!-- title -->
+    <title>Shop Giao</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta name="author" content="giaodaica">
+    <!-- description -->
+    <meta name="description" content="Mua quần áo thời trang đẹp, chất lượng tại giaoshop. Cập nhật xu hướng mới nhất với áo thun, váy, quần jeans, phụ kiện phong cách.">
+    <meta name="keywords" content="thời trang, quần áo, cửa hàng thời trang, xu hướng thời trang, áo thun, váy đẹp, quần jeans, phụ kiện thời trang, streetwear, boutique, phong cách, quần áo giá rẻ, thời trang nam, thời trang nữ">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon-32.png')}}">
+    <link rel="apple-touch-icon" href="{{asset('assets/images/favicon-57.png')}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('assets/images/favicon-72.png')}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('assets/images/favicon-144.png')}}">
+    <!-- google fonts preconnect -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- style sheets and font icons  -->
+    <link rel="stylesheet" href="{{asset('assets/css/vendors.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/icon.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}"/>
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}"/>
+</head>
---
+++ resources/views/card/js.blade.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="{{asset('assets/js/jquery.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/js/vendors.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/js/main.js')}}"></script>
+
