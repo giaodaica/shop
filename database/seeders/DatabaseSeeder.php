2efe50147b44174bdb267274781511e7e92648a3
phuduc2003 <phulinhk8@gmail.com>
2025-05-13 21:45:13 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-//Chu thich
+//Chu thich abc
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
