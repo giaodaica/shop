2c34115c2744166a7cf605de421317f531d64148
phuduc2003 <phulinhk8@gmail.com>
2025-05-20 19:36:51 +0700

    Client create detail

--- resources/views/dashboard/product/index.blade.php
+++ resources/views/dashboard/product/index.blade.php
@@ -296,7 +296,7 @@
                             <div class="row g-4">
                                 <div class="col-sm-auto">
                                     <div>
-                                        <a href="apps-ecommerce-add-product.html" class="btn btn-success" id="addproduct-btn"><i class="ri-add-line align-bottom me-1"></i> Add Product</a>
+                                        <a href="{{ route('products.create') }}" class="btn btn-success" id="addproduct-btn"><i class="ri-add-line align-bottom me-1"></i> Add Product</a>
                                     </div>
                                 </div>
                                 <div class="col-sm">
