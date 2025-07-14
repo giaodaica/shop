a45770bd2ff6735a5456ad4d8785f1ac44042c4b
Nguyễn Anh Quân <167054493+aquan17@users.noreply.github.com>
2025-07-14 21:36:39 +0700

    fix lai xem bill

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -310,6 +310,7 @@ class OrderController extends Controller
 
         $before = $request->change;
         // dd($before);
+        // dd($request->all());
         $request->validate(
             [
                 'content' => 'nullable|string|max:255',
