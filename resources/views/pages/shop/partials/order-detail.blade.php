2782fb7dac6792bc0016e0bff14c303d2c8002b5
Nguyễn Anh Quân <167054493+aquan17@users.noreply.github.com>
2025-08-20 16:55:01 +0700

    sua cot so luong chi tiet don hang

--- resources/views/pages/shop/partials/order-detail.blade.php
+++ resources/views/pages/shop/partials/order-detail.blade.php
@@ -156,7 +156,9 @@
                                 <tr>
                                     <th>STT</th>
                                     <th>Chi tiết sản phẩm</th>
+                                    <th>Số lượng</th>
                                     <th>Giá</th>
+
                                 </tr>
                             </thead>
                             <tbody>
@@ -174,8 +176,8 @@
                                                 <div style="font-size: 13px; color: #555;">
                                                     {{ $item->productVariant->color->color_name ?? $item->color_name }},
                                                     {{ $item->productVariant->size->size_name ?? $item->size_name }}
-                                                    <br>
-                                                    x{{ $item->quantity }}
+                                                    {{-- <br> --}}
+                                                    
                                                 </div>
                                                 @php
                                                     $orderStatus = $order->status ?? null;
@@ -201,6 +203,7 @@
                                                 @endif
                                             </div>
                                         </td>
+                                        <td>{{ $item->quantity }}</td>
                                         <td>{{ number_format($item->sale_price) }}đ</td>
                                     </tr>
                                     <div class="modal fade" id="reviewModal-{{ $item->id }}" tabindex="-1"
